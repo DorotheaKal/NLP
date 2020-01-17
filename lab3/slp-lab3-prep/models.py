851719c3d5be94d98a3ca651cd5123af6db614d3
nikitas-theo <nikitastheodorop@gmail.com>
2020-01-17 18:38:05 +0200

    Added attention

--- lab3/slp-lab3-prep/models.py
+++ lab3/slp-lab3-prep/models.py
@@ -140,3 +140,57 @@ class BaselineDNN(nn.Module):
             logits = logits.view((-1)).float()
         
         return logits
+
+class BaseLSTM(nn.Module):
+    def __init__(self,output_size, embeddings,hidden = 8, trainable_emb=False):
+
+        super(BaseLSTM, self).__init__()
+        
+        embeddings = np.array(embeddings)
+        num_embeddings, dim = embeddings.shape 
+        
+        self.embeddings = nn.Embedding(num_embeddings,dim)
+        self.output_size = output_size
+        
+        self.lstm = nn.LSTM(dim,hidden_size = hidden)
+        if not trainable_emb:
+            self.embeddings = self.embeddings.from_pretrained(torch.Tensor(embeddings), freeze = True)
+
+        if output_size == 2 :
+            output_size = 1
+    
+        # 3*HS , h_t_last || mean(h_t) || max(h_t)
+        self.linear = nn.Linear(3*hidden,output_size)
+    
+    def forward(self,x,lengths):
+        embeddings = self.embeddings(x) # :: BS x SEQ_LEN x EMB_DIM 
+        
+        # LSTM requires input SEQ_LEN x BS x EMB_DIM
+        ht,_, = self.lstm(torch.transpose(embeddings, 0, 1))
+        # ht :: SEQ_LEN x BS x HS
+        
+        ## Need to index by lengths, drop unwanted hidden states...
+        
+        # ht_last :: BS x HS
+        ht_last = torch.zeros((ht.shape[1],ht.shape[2]))
+        
+        for i in range(ht.shape[1]):
+            if (lengths[i] > ht.shape[0]):
+                lengths[i] = ht.shape[0]
+
+            ht[lengths[i]:,i,:] = 0 # zero unwanted states
+            ht_last[i] = ht[lengths[i]-1,i,:] # save last hidden
+
+        ht =  torch.transpose(ht,0,1) # transpose back to previous shape
+       
+        mean = torch.sum(ht,axis = 1) # BS x HS 
+        mean = mean / lengths.view((-1,1)) 
+        max_vals,_ = torch.max(ht,axis = 1) # BS x HS
+        rep = torch.cat( (mean,max_vals) ,dim = 1) # BS x 2HS
+        rep = torch.cat( (ht_last, rep),dim = 1) # BS x 3HS
+        out = self.linear(rep)
+        
+        if self.output_size == 2:
+            out = out.view((-1)).float()
+        
+        return out
